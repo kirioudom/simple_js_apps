c7d8b07a71803b834c4fbc5902e35d0c0e820aa2
kiry <kirioudom086@gmail.com>
2021-08-12 00:20:54 +0700

    book keeper add feature

--- book_keeper/script.js
+++ book_keeper/script.js
@@ -1,30 +1,100 @@
-function modalHandler() {
+function modalInit(submitHandler) {
   const modalElement = document.querySelector("#modal");
   const backdrop = document.querySelector("#backdrop");
+  const clearInputForm = formInit(submitHandler);
 
   const modalTrigger = () => {
+    clearInputForm();
     modalElement.classList.toggle("open-modal");
     backdrop.classList.toggle("open-modal");
-    //click on bookmark btn open backdrop and modal
-    //click on backdrop close the backdrop and modal
   };
+
+  document.querySelector("#btn-bookmark").addEventListener("click", (e) => {
+    modalTrigger();
+  });
 
   backdrop.addEventListener("click", (e) => {
     modalTrigger();
   });
-
-  return modalTrigger;
 }
 
-function formHandler() {
-  const formElement = document.querySelector("#formSubmit");
+function formInit(submitHandler) {
+  const inputWebsiteNameElment = document.querySelector("#website-name");
+  const inputWebsiteLinkElement = document.querySelector("#website-link");
+  const formElement = document.querySelector("form");
+  const submitBtn = document.querySelector("#btn-bookmark");
+  let websiteName = "";
+  let websiteLink = "";
 
-  const 
+  inputWebsiteLinkElement.addEventListener("input", (e) => {
+    websiteLink = e.target.value;
+  });
+
+  inputWebsiteNameElment.addEventListener("input", (e) => {
+    websiteName = e.target.value;
+  });
+
+  formElement.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if ([websiteName, websiteLink].filter(Boolean).length === 2) {
+      submitHandler({ websiteLink, websiteName });
+      clearInputForm();
+    }
+  });
+
+  submitBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+  });
+
+  const clearInputForm = () => {
+    websiteName = "";
+    websiteLink = "";
+    inputWebsiteNameElment.value = websiteName;
+    inputWebsiteLinkElement.value = websiteLink;
+  };
+
+  return clearInputForm;
+}
+
+function bookKeeperData() {
+  const dataKey = "bookmarks";
+  let bookmarks = localStorage.getItem(dataKey)
+    ? JSON.parse(localStorage.getItem(dataKey))
+    : [];
+  console.log(bookmarks);
+  return {
+    getBookmarks: () => bookmarks,
+    addBookmark: ({ websiteName, websiteLink }) => {
+      bookmarks = [...bookmarks, { websiteName, websiteLink }];
+      localStorage.setItem(dataKey, JSON.stringify(bookmarks));
+      return true;
+    },
+    removeBookmark: (websiteName) => {
+      bookmarks = bookmarks.filter(
+        (bookmark) => bookmark.websiteName === websiteName
+      );
+      localStorage.setItem(dataKey, bookmarks);
+      return true;
+    },
+  };
+}
+
+function updateBookmarksView(data) {
+  const containerElement = document.querySelector(".container");
+  const generateLinkElment = ({ websiteName, websiteLink }) => {
+    return `<div class='link-container'>
+    <a href=${websiteLink} class='bookmark-link'>${websiteName}</a>
+    </div>`;
+  };
+
+  containerElement.innerHTML = data.reduce((element, bookmarkInfo) => {
+    console.log({ ...bookmarkInfo });
+    return (element += generateLinkElment(bookmarkInfo));
+  }, "");
 }
 
 window.onload = () => {
-  const modalTrigger = modalHandler();
-  document.querySelector("#btn-bookmark").addEventListener("click", (e) => {
-    modalTrigger();
-  });
+  const bookmarkProvier = bookKeeperData();
+  modalInit(bookmarkProvier.addBookmark);
+  localStorage.clear();
 };
