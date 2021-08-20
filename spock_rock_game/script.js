ba08a007aef9321a34dce7752406b6745b2e0170
kiry <kirioudom086@gmail.com>
2021-08-20 14:14:28 +0700

    config some files

---
+++ spock_rock_game/script.js
@@ -0,0 +1,17 @@
+function main() {
+  const gameResult = {
+    lost: "You lost!",
+    tie: "It's tie!",
+    win: "Congrats! you won!",
+  };
+
+  const gameRules = {
+    paper: ["rock", "spock"],
+    spock: {},
+    scissors: {},
+    rock: {},
+    lizard: {},
+  };
+}
+
+window.onload = main;
