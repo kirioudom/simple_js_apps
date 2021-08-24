030ba2da2349350df0a7b272410b5a4e69498b0a
kiry <kirioudom086@gmail.com>
2021-08-24 23:14:40 +0700

    finished functions of rock paper game

--- spock_rock_game/script.js
+++ spock_rock_game/script.js
@@ -1,40 +1,92 @@
-function gameView() {
-  const playerSymbolName = document.querySelector("#player-symbol");
-  const computerSymbolName = document.querySelector("#computer-symbol");
-
+function GameView() {
+  const playerSymbolNameElement = document.querySelector("#player-symbol");
+  const computerSymbolNameElement = document.querySelector("#computer-symbol");
+  const gameResultElement = document.querySelector("#resultGame");
   const updatePlayerSymbolName = (symbolName) =>
-    (playerSymbolName.innerText = symbolName);
+    (playerSymbolNameElement.innerText = symbolName);
 
   const updateComputerSymbolName = (symbolName) =>
-    (computerSymbolName.innerText = symbolName);
+    (computerSymbolNameElement.innerText = symbolName);
 
   const resetView = () => {
-    playerSymbolName.innerText = "";
-    computerSymbolName.innerText = "";
+    playerSymbolNameElement.innerText = "";
+    computerSymbolNameElement.innerText = "";
+
+    gameResultElement.classList.add("hide");
   };
 
-  return { resetView, updateComputerSymbolName, updatePlayerSymbolName };
+  const updateGameResult = (msg) => {
+    if (gameResultElement.classList.contains("hide")) {
+      gameResultElement.classList.remove("hide");
+    }
+    gameResultElement.innerText = msg;
+  };
+
+  return {
+    resetView,
+    updateComputerSymbolName,
+    updatePlayerSymbolName,
+    updateGameResult,
+  };
 }
 
-function controller(handler) {
+function playerController(handler) {
   const symbolButtons = document.querySelectorAll("#controllerSymbol");
 
-  const resetFocus = () => {
+  const focusOnSymbol = (symbolName) => {
     symbolButtons.forEach((symbolButton) => {
+      if (symbolButton.dataset.name === symbolName) {
+        symbolButton.classList.add("btn-symbol-focus");
+        return;
+      }
       symbolButton.classList.remove("btn-symbol-focus");
+    });
+  };
+
+  const resetPlayerController = () => {
+    symbolButtons.forEach((symbol) => {
+      symbol.classList.remove("btn-symbol-focus");
     });
   };
 
   symbolButtons.forEach((symbolButton) => {
     symbolButton.addEventListener("click", (event) => {
-      resetFocus();
-      handler(symbolButton.dataset.name);
-      symbolButton.classList.add("btn-symbol-focus");
+      focusOnSymbol(event.target.dataset.name);
+      handler(event.target.dataset.name);
     });
   });
+
+  return resetPlayerController;
 }
 
-function computerController() {}
+function ComputerController() {
+  const controllerSymbolsElements = document.querySelectorAll(
+    "#computerPlayerSymbol"
+  );
+
+  const focusOnSymbol = (symbol) => {
+    controllerSymbolsElements.forEach((computerPlayerSymbol) => {
+      if (computerPlayerSymbol.dataset.name === symbol) {
+        computerPlayerSymbol.classList.add("computer-player-symbol-focus");
+        return;
+      }
+      computerPlayerSymbol.classList.remove("computer-player-symbol-focus");
+    });
+  };
+
+  return {
+    computerControllerReset: () => {
+      controllerSymbolsElements.forEach((symbolEle) => {
+        symbolEle.classList.remove("computer-player-symbol-focus");
+      });
+    },
+    randomlySelectSymbol: (symbols) => {
+      const selectedSymbol = ramdomSelectedSymbol(symbols);
+      focusOnSymbol(selectedSymbol);
+      return selectedSymbol;
+    },
+  };
+}
 
 function ramdomSelectedSymbol(symbols) {
   const ramdomIndex = Math.floor(Math.random() * symbols.length);
@@ -57,9 +109,36 @@ function game() {
   };
 
   const symbols = Object.keys(gameRules);
-  const gameView = gameView();
+  const gameView = GameView();
+  const computerController = ComputerController();
 
-  controller((symbolName) => {});
+  const resetPlayerController = playerController(function selectSymbolHandler(
+    playerSelectedSymbol
+  ) {
+    const playerSelectedSymbolInfo = gameRules[playerSelectedSymbol];
+    const computerSelectedSymbol =
+      computerController.randomlySelectSymbol(symbols);
+
+    gameView.updatePlayerSymbolName(playerSelectedSymbol);
+    gameView.updateComputerSymbolName(computerSelectedSymbol);
+
+    if (playerSelectedSymbolInfo.beat.includes(computerSelectedSymbol)) {
+      gameView.updateGameResult(gameResult.win);
+      return;
+    }
+    if (playerSelectedSymbolInfo.tie === computerSelectedSymbol) {
+      gameView.updateGameResult(gameResult.tie);
+      return;
+    }
+
+    gameView.updateGameResult(gameResult.lost);
+  });
+
+  document.querySelector("#resetGame").addEventListener("click", (event) => {
+    gameView.resetView();
+    computerController.computerControllerReset();
+    resetPlayerController();
+  });
 }
 
 window.onload = game;
